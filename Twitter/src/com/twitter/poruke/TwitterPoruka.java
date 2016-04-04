52ea50590962d07b24bdeb45bd6fd03e6bbaeca5
stefanabogdanovic95 <stefibog95@hotmail.com>
2016-04-03 18:03:28 -0700

    Uradjena je JavaDoc dokumentacija za klasu TwitterPoruka, njene atribute i metode.

--- Twitter/src/com/twitter/poruke/TwitterPoruka.java
+++ Twitter/src/com/twitter/poruke/TwitterPoruka.java
@@ -1,33 +1,91 @@
 package com.twitter.poruke;
 
+/**
+ * Klasa predstavlja twitter poruku.
+ * 
+ * @author Stefana Bogdanovic
+ * @version 0.1
+*/
 public class TwitterPoruka {
 	
-private String korisnik;
-private String poruka;
 
-public String getKorisnik() {
-return korisnik;
-}
+	/**
+	 * Atribut koji predstavlja ime korisnika
+	 */
+	private String korisnik;
 
-public void setKorisnik(String korisnik) {
-if (korisnik==null || korisnik == "")
-throw new RuntimeException(
-"Ime korisnika mora biti uneto");
-this.korisnik = korisnik;
-}
+	/**
+	 * Atribut koji predstavlja sadrzaj poruke
+	 */
+	private String poruka;
 
-public String getPoruka() {
-return "poruka";
-}
 
-public void setPoruka(String poruka) {
-if (poruka==null || this.poruka.length()>140)
-throw new RuntimeException(
-"Poruka mora biti uneta i mora imati najvise 140 znakova");
-this.poruka = poruka;
-}
+	/**
+	 * Metoda vraca ime korisnika
+	 * @return ime korisnika
+	 */
+	public String getKorisnik() {
 
-public String toString(){
-return "KORISNIK:"+korisnik+" PORUKA:"+poruka;
-}
+		return korisnik;
+
+	}
+
+
+	/**
+	 * Metoda postavlja vrednost atributa 'korisnik' na vrednost zadatog parametra
+	 * @param korisnik ime korisnika
+	 * @throws java.lang.RuntimeException ako je vrednost unetog parametra null ili prazan string
+	 * 
+	 */
+	public void setKorisnik(String korisnik) {
+
+		if (korisnik==null || korisnik == "")
+
+			throw new RuntimeException(
+
+					"Ime korisnika mora biti uneto");
+
+		this.korisnik = korisnik;
+
+	}
+
+
+	/**
+	 * Metoda vraca sadrzaj poruke
+	 * @return twitter poruka
+	 */
+	public String getPoruka() {
+
+		return "poruka";
+
+	}
+
+
+	/**
+	 * Metoda postavlja vrednost atributa 'poruka' na zadatu vrednost
+	 * @param poruka sadrzaj poruke
+	 * @throws java.lang.RuntimeException ako je vrednost unetog parametra null ili je duzina stringa veca od 140
+	 */
+	public void setPoruka(String poruka) {
+
+		if (poruka==null || this.poruka.length()>140)
+
+			throw new RuntimeException(
+
+					"Poruka mora biti uneta i mora imati najvise 140 znakova");
+
+		this.poruka = poruka;
+
+	}
+
+
+	/**
+	 * Redefinisana metoda 'toString'
+	 * @return ime korisnika i poruku u zadatom formatu
+	 */
+	public String toString(){
+
+		return "KORISNIK:"+korisnik+" PORUKA:"+poruka;
+
+	}
 }
